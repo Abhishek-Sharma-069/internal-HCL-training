309de7f378651b876740461b6a674697fd38aada
Abhishek Sharma <abhello12@gmail.com>
2025-12-10 04:57:40 +0000

    basic funtions of array

--- Day-3/day3.js
+++ Day-3/day3.js
@@ -13,3 +13,22 @@ console.log(fun(72));
 let arr = ["King", 1, 3, 0.9, fun(10)];
 
 console.log(arr);
+
+
+// ------------------------------------------ Basic Functions of array-------------
+
+fruits = ["Apple", "Strawberry", "Pineapple","Gwava"];
+
+fruits.forEach(element => {
+    console.log(element);
+});
+
+
+fruits.push("papaya");
+fruits.pop();
+
+fruits.unshift("kiwi");
+
+console.log(fruits.includes("Mango"));
+
+console.log(fruits.indexOf("Banana"));
